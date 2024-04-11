172d7447cbe9190b6bce9e2e4770a1bee3914f2c
Patricia Magalhães <71184621+pmagalhaes2@users.noreply.github.com>
2024-04-11 12:43:02 -0300

    chore: add column active

--- src/main/java/com/example/crud/controllers/ProductController.java
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -3,9 +3,9 @@ package com.example.crud.controllers;
 import com.example.crud.domain.product.Product;
 import com.example.crud.domain.product.ProductRepository;
 import com.example.crud.domain.product.RequestProductDTO;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -50,6 +50,7 @@ public class ProductController {
     }
 
     @PutMapping("/{id}")
+    @Transactional
     public ResponseEntity updateProduct(@RequestBody @Valid RequestProductDTO data, @PathVariable UUID id) {
         try {
             Optional<Product> product = repository.findById(String.valueOf(id));
